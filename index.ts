b2a19dabe90b88e40ad0ee0cd5e80416faf822e6
M.D. Walters <808426@pdsb.net>
2022-12-08 18:40:19 +0000

    This *is* a rewrite after all TYPESCRIPT JUST ADDS STUFF TO JAVASCRIPT YOU D-

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ import { v4 as uuid } from "uuid";
 const wss = new WebSocketServer({ port: 8080 });
 const db = new JSONdb("db.json");
 
-wss.on("connection", function connection(ws) {
-    ws.on("message", function message(data: any) {
+wss.on("connection", (ws) => {
+    ws.on("message", (data: any) => {
         var data: any = JSON.parse(data);
         console.log(`New message: ${data}`);
         if (data.cmd === "post") {
@@ -21,13 +21,13 @@ wss.on("connection", function connection(ws) {
                 }
             }
             
-            bcrypt.compare(data.password, users[i].password, function(err, result) {
+            bcrypt.compare(data.password, users[i].password, (err, result) => {
                 if (result == true) {
                     ws.send(JSON.stringify({"cmd": "ok"}));
                     var home = db.get("_home");
                     home.push({"username": JSON.parse(data).username, "content": JSON.parse(data).val, "uuid": uuid(), "created": new Date().getTime()});
                     db.set("_home", home);
-                    wss.clients.forEach(function each(client) {
+                    wss.clients.forEach((client) => {
                         client.send(JSON.stringify({"cmd": "message", "username": JSON.parse(data).username, "val": JSON.parse(data).val}));
                     });
                 } else {
@@ -48,7 +48,7 @@ wss.on("connection", function connection(ws) {
             if (hasUser) {
                 ws.send(JSON.stringify({"cmd": "status", "val": "Account Exists"}));
             } else {
-                bcrypt.hash(data.password, 14, function(err, hash) {
+                bcrypt.hash(data.password, 14, (err, hash) => {
                     users.push({"username": data.username, "password": hash, "uuid": uuid(), "created": new Date().getTime()});
                     db.set("_users", users);
                     ws.send(JSON.stringify({"cmd": "ok"}));
@@ -68,7 +68,7 @@ wss.on("connection", function connection(ws) {
             if (!(hasUser)) {
                 ws.send(JSON.stringify({"cmd": "error", "val": "Invalid Username or Password"}));
             } else {
-                bcrypt.compare(data.password, db.get("_users")[i].password, function(err, result) {
+                bcrypt.compare(data.password, db.get("_users")[i].password, (err, result) => {
                     if (result == true) {
                         ws.send(JSON.stringify({"cmd": "ok"}));
                     } else {
