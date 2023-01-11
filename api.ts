c4ac551041b46dbb9b4c3f44b03ffdfe42f0a2c1
M.D. Walters <808426@pdsb.net>
2023-01-11 16:02:01 +0000

    Yes

--- api.ts
+++ api.ts
@@ -2,9 +2,12 @@ import express from "express";
 import JSONdb from "simple-json-db";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import Gocial from "./gocial";
 
 const app = express();
 const db = new JSONdb("db.json");
+const gocial = new Gocial();
+
 const port: number = 3000;
 
 app.get("/", (req, res) => {
@@ -12,11 +15,13 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create/post", (req, res) => {
-    let username: string = res.getHeader("username");
-    let password: string = res.getHeader("password");
-    let content: string = res.getHeader("content");
+    let username = res.get("username");
+    let password = res.get("password");
+    let content = res.get("content");
 
-    
+    if (gocial.hasUser(username)) {
+        
+    }
 });
 
 app.listen(port);
