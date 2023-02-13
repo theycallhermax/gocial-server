edc6da7fd926da7a55b3a419f7e0dec5db64db3b
M.D. Walters <808426@pdsb.net>
2023-02-13 01:03:32 +0000

    aa

--- index.ts
+++ index.ts
@@ -11,11 +11,11 @@ const port: number = 3000;
 * Checks to see if the specified user exists
 * @param user The username to check if the user exists
 */
-function hasUser(user: any): null | Object {
+function hasUser(user: any): any {
     let users = db.get("_users");
     let i: number = 0;
     for (i < users.length; i++;) {
-        if (users[i].username === user) {
+        if (users[i].username == user) {
             return users[i];
         }
     }
@@ -27,10 +27,12 @@ app.get("/", (req, res) => {
         "users": db.get("_users").length,
         "posts": db.get("_home").length
     });
+    res.end();
 });
 
 app.get("/home", (req, res) => {
     res.status(200).send(db.get("_home"));
+    res.end();
 });
 
 app.post("/home/post", (req, res) => {
@@ -40,7 +42,7 @@ app.post("/home/post", (req, res) => {
         if (user) {
             // @ts-ignore
             bcrypt.compare(req.headers.password, user.password, (err, result) => {
-                if (result == true) {
+                if (result) {
                     home.push({
                         "username": req.headers["username"],
                         "content": req.headers["content"],
@@ -51,11 +53,13 @@ app.post("/home/post", (req, res) => {
                     res.status(201).send({
                         "error": false
                     });
+                    res.end();
                 } else {
                     res.status(403).send({
                         "error": true,
                         "message": "Invalid Password"
                     });
+                    res.end();
                 }
             });
         } else {
@@ -63,12 +67,29 @@ app.post("/home/post", (req, res) => {
                 "error": true,
                 "message": "Invalid Username"
             });
+            res.end();
         }
     } catch(e) {
         res.status(500).send({
             "error": true,
             "message": "Internal Server Error"
         });
+    }
+});
+
+app.get("/users/:user", (req, res) => {
+    let user = hasUser(req.params.user);
+    if (user) {
+        // @ts-ignore
+        delete user.password;
+        res.status(200).send(user);
+        res.end();
+    } else {
+        res.status(500).send({
+            "error": true,
+            "message": "Not Found"
+        });
+        res.end();
     }
 });
 
