b3b813f5a42f4d5e68e8811eb0dd119d948e33b9
M.D. Walters <popularvsofficial@gmail.com>
2023-02-24 19:41:48 -0500

    yes

--- index.ts
+++ index.ts
@@ -7,21 +7,6 @@ const app = express();
 const db = new JSONdb("db.json");
 const port: number = 3000;
 
-/**
-* Checks to see if the specified user exists
-* @param user The username to check if the user exists
-*/
-function hasUser(user: any): any {
-    let users = db.get("_users");
-    let i: number = 0;
-    for (i < users.length; i++;) {
-        if (users[i].username == user) {
-            return users[i];
-        }
-    }
-    return null;
-}
-
 app.get("/", (req, res) => {
     res.status(200).send({
         "users": db.get("_users").length,
@@ -36,12 +21,20 @@ app.get("/home", (req, res) => {
 });
 
 app.post("/home/post", (req, res) => {
+    let users = db.get("_users");
+    let hasUser: boolean = false;
+    let i: number = 0;
+    for (i < users.length; i++;) {
+        if (users[i].username === data.username) {
+            hasUser = true;
+            break;
+        }
+    }
+
     let home = db.get("_home");
-    let user = hasUser(req.headers["username"]);
     try {
-        if (user) {
-            // @ts-ignore
-            bcrypt.compare(req.headers.password, user.password, (err, result) => {
+        if (hasUser) {
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
                 if (result) {
                     home.push({
                         "username": req.headers["username"],
@@ -77,6 +70,7 @@ app.post("/home/post", (req, res) => {
     }
 });
 
+/*
 app.get("/users/:user", (req, res) => {
     let user = hasUser(req.params.user);
     if (user) {
@@ -92,6 +86,7 @@ app.get("/users/:user", (req, res) => {
         res.end();
     }
 });
+*/
 
 app.listen(port, () => {
     console.log(`Gocial server listening on port ${port}`);
